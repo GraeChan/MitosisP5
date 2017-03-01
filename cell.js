72937e8e2f65361eb37ba73f49357174fa896f0e
GraeChan <gabsturr@hotmail.com>
2017-03-01 23:09:57 +0000

    Added Chromosome.

--- cell.js
+++ cell.js
@@ -1,4 +1,4 @@
-function Cell(pos, r, c) {
+function Cell(pos, r, c, ic) {
 
   if (pos) {
     this.pos = pos.copy();
@@ -6,8 +6,9 @@ function Cell(pos, r, c) {
     this.pos = createVector(random(width), random(height));
   }
 
-  this.r = r || 60;
+  this.r = r || 100;
   this.c = c || color(random(50, 255), random(50, 255), random(50, 255), 100);
+  this.ic = ic || color(random(50, 255), random(50, 255), random(50, 255), 100);
 
   this.clicked = function(x, y) {
     var d = dist(this.pos.x, this.pos.y, x, y);
@@ -20,7 +21,7 @@ function Cell(pos, r, c) {
 
   this.mitosis = function() {
     //this.pos.x += random(-this.r, this.r);
-    var cell = new Cell(this.pos, this.r*0.75, this.c);
+    var cell = new Cell(this.pos, this.r*0.75, this.c, this.ic);
     return cell;
   }
 
@@ -33,6 +34,12 @@ function Cell(pos, r, c) {
     noStroke();
     fill(this.c);
     ellipse(this.pos.x, this.pos.y, this.r, this.r)
+	
+	fill(this.ic);
+	//strokeWeight(1);
+	//stroke(random(50, 255), random(50, 255), random(50, 255));
+	
+	ellipse(this.pos.x, this.pos.y, this.r/2, this.r/2)
   }
 
 }
